0e2ffb6216a56be9ce7289af455357c32d0ebfaa
z <z@gmail.com>
2025-09-10 08:13:11 +0300

    add Video

--- home-manager/zerg/packages.nix
+++ home-manager/zerg/packages.nix
@@ -1,61 +1,47 @@
 { config, pkgs, pkgs-unstable, nix-ai-tools, ... }:
-
+let
+  aiTools = nix-ai-tools.packages.${pkgs.system};
+in
 {
-  programs.vesktop.enable = true;
+  # programs.vesktop.enable = true;
 
   home.packages = with pkgs; [
-
     # Base Utils
-    neovim wget curl git btop microfetch fastfetch tmux
+    neovim wget curl git btop tmux
+    microfetch fastfetch
 
     # Code editor
+    neovim
+    unstable.zed-editor-fhs
     unstable.vscode-fhs
-    unstable.zed-editor
+    unstable.kiro-fhs
 
     # Massenger
-    telegram-desktop
-    vesktop
+    telegram-desktop vesktop
 
     # Terminal
-    alacritty
-    wezterm
+    alacritty wezterm
 
     # Browser
-    librewolf
-    brave
+    librewolf brave
 
     # Video
+    vlc
     unstable.kdePackages.kdenlive
 
     # Git утилиты
-    jujutsu
-    lazyjj
+    git lazygit
+    jujutsu lazyjj
 
     # AI инструменты
-    # из nix-ai-tools ${pkgs.system} = x86_64-linux
-    nix-ai-tools.packages.x86_64-linux.opencode
-    nix-ai-tools.packages.x86_64-linux.qwen-code
-    nix-ai-tools.packages.x86_64-linux.gemini-cli
-    nix-ai-tools.packages.x86_64-linux.claude-code-router
-    nix-ai-tools.packages.x86_64-linux.crush
-
-   # git
-   # vim
-  ] ++ [
-    # Пакеты из unstable
-   # unstable.zed-editor-fhs  # используем unstable вместо pkgs
-
+    aiTools.opencode
+    aiTools.qwen-code
+    aiTools.gemini-cli
+    aiTools.claude-code-router
+    aiTools.crush
   ];
 
-  # home.packages = with inputs.nix-ai-tools.packages.x86_64-linux; [ # ${pkgs.system} # AI tools
-  #   qwen-code
-  #   gemini-cli
-  #   crush
-  #   claude-code-router
-  #   # ... other tools
-  # ];
   # Настройки shell
-
   programs = {
     bash.enable = true;
     zsh.enable = true;
