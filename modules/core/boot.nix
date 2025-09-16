217e4e5ac5a3ab50f8e07c4fb6518cf123f95662
z <z@gmail.com>
2025-09-17 00:05:06 +0300

    kernel 6.16

--- modules/core/boot.nix
+++ modules/core/boot.nix
@@ -1,9 +1,10 @@
 { config, pkgs, ... }:
 
 {
-  boot.kernelPackages = pkgs.linuxPackages_6_16;
   
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  
+  boot.kernelPackages = pkgs.linuxPackages_6_16;
 }
